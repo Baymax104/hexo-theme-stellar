33c6e5e1ca742655cc5b487c7070c6b29b2669a6
John Wei <wzy1048168235@163.com>
2025-08-30 01:21:39 +0800

    feat(main): 更新主题

--- scripts/generators/search.js
+++ scripts/generators/search.js
@@ -4,34 +4,34 @@
 const { stripHTML } = require('hexo-util')
 
 hexo.extend.generator.register('search_json_generator', function (locals) {
-  if (this.theme.config.search.service != 'local_search') { return {} }
+  if (this.theme.config.search.service !== 'local_search') { return {} }
   const { root } = this.config
   const { local_search: cfg } = this.theme.config.search
   cfg.sort = '-date'
   cfg.field = cfg.field?.trim()
 
-  var posts, pages
-  if (cfg.field == 'post') {
+  let posts, pages;
+  if (cfg.field === 'post') {
     posts = locals.posts?.filter(p => p.content?.length > 0).sort(cfg.sort)
-  } else if (cfg.field == 'page') {
+  } else if (cfg.field === 'page') {
     pages = locals.pages?.filter(p => p.content?.length > 0)
   } else {
     posts = locals.posts?.filter(p => p.content?.length > 0).sort(cfg.sort)
     pages = locals.pages?.filter(p => p.content?.length > 0)
   }
 
-  var res = new Array()
+  const res = [];
 
   function generateJson(post) {
-    var temp_post = new Object()
+    const temp_post = {};
     if (post.title) {
       temp_post.title = post.title.trim()
     }
     if (post.path) {
       temp_post.path = root + post.path
     }
-    if (cfg.content != false && post.content) {
-      var content = stripHTML(post.content.replace(/<span class="line">\d+<\/span>/g, '')).trim()
+    if (cfg.content !== false && post.content) {
+      let content = stripHTML(post.content.replace(/<span class="line">\d+<\/span>/g, '')).trim();
       // 部分HTML标签
       content = content.replace(/<iframe[\s|\S]+iframe>/g, '')
       content = content.replace(/<hr>/g, '')
@@ -42,18 +42,18 @@ hexo.extend.generator.register('search_json_generator', function (locals) {
       content = content.replace(/\\n/g, ' ')
       content = content.replace(/\n/g, ' ')
       // 多个连续空格换成单个空格
-      content = content.replace(/[\s]{2,}/g, ' ')
+      content = content.replace(/\s{2,}/g, ' ')
       temp_post.content = content.trim()
     }
     if (post.tags && post.tags.length > 0) {
-      var tags = []
+      const tags = [];
       post.tags.forEach(function (tag) {
         tags.push(tag.name)
       })
       temp_post.tags = tags
     }
     if (post.categories && post.categories.length > 0) {
-      var categories = []
+      const categories = [];
       post.categories.forEach(function (cate) {
         categories.push(cate.name)
       })
@@ -75,20 +75,20 @@ hexo.extend.generator.register('search_json_generator', function (locals) {
 
   if (posts) {
     posts.each(function(post) {
-      var layout_list = ["post"]
+      const layout_list = ["post"];
       if (!layout_list.includes(post.layout)) return
       if (cfg.skip_search && matchAndExit(post.path, cfg.skip_search)) return
-      if (post.indexing == false) return
+      if (post.indexing === false) return
       let temp_post = generateJson(post)
       res.push(temp_post)
     }) 
   } 
   if (pages) {
     pages.each(function(page) {
-      var layout_list = ["page", "wiki"]
+      const layout_list = ["page", "wiki"];
       if (!layout_list.includes(page.layout)) return
       if (cfg.skip_search && matchAndExit(page.path, cfg.skip_search)) return
-      if (page.indexing == false) return
+      if (page.indexing === false) return
       let temp_post = generateJson(page)
       res.push(temp_post)
     })
